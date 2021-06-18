c32916251a27a7ff6ee1c06349ee919d9f7d15da
Stick cruz <stick.cruz1992@gmail.com>
2021-06-18 13:38:37 -0500

    first commit

--- service-shopping/src/main/java/com/example/shopping/client/CustomerClient.java
+++ service-shopping/src/main/java/com/example/shopping/client/CustomerClient.java
@@ -6,13 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.shopping.model.Customer;
 
 //@FeignClient(name = "service-customer", url="http://localhost:8081/v1/customers", fallback = CustomerHystrixFallbackFactory.class)
-//@RequestMapping("/customers")
-@FeignClient(name = "service-customer", url="http://localhost:8081/v1/customers")
+@FeignClient(name = "service-customer", url="http://localhost:8081/v1/customers", fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {
 
     @GetMapping(value = "/{id}")
